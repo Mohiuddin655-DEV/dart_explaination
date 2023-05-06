f1400972cb3de18a222bc268afa0b2e5f6719ecd
Md. Mohi-Uddin <mohiuddin655.bd@gmail.com>
2023-05-06 11:49:19 +0600

    method

--- bin/dart_explanation.dart
+++ bin/dart_explanation.dart
@@ -1,6 +1,13 @@
 import 'package:dart_explanation/dart_class.dart';
 
 void main(List<String> arguments) {
-  var tester = Tester();
-  tester.sayHello();
+
+  var tester = Tester(); // Create a new tester class
+
+  tester.sayHello(); // Simple method call
+
+  tester.sayHelloWith("Hello world!"); // Parameterized method call
+
+  var message = tester.getMessage(); // Return type method call
+  print(message); // print returnable data
 }
--- lib/dart_class.dart
+++ lib/dart_class.dart
@@ -4,4 +4,14 @@ class Tester {
   void sayHello() {
     print("Hello world!");
   }
+
+  // This is a parameterized method
+  void sayHelloWith(String message) {
+    print("This is a parameterized method with message : $message");
+  }
+
+  // This is a return type method
+  String getMessage() {
+    return "This is a return type method!";
+  }
 }
